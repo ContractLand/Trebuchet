aa11712d6357093b07dfd58b7425d6f63fd332f0
Raymond Yeh <yehjxraymond@gmail.com>
2018-12-04 16:35:52 +0800

    Added test for process

--- packages/hive-vu-eth/src/vu.test.js
+++ packages/hive-vu-eth/src/vu.test.js
@@ -1,3 +1,4 @@
+const sinon = require("sinon");
 const VU = require("./vu");
 
 // Test parameters
@@ -206,6 +207,51 @@ describe("class methods", () => {
       };
       const res = await vu.requestMinFund("999");
       expect(res).toBe("1000");
+    });
+  });
+
+  describe("txWrapper", () => {
+    test("should wrap a function, and time it's execution", async () => {
+      const reportedName = "TestAsyncFn";
+      const timeout = 100;
+      const returnedResults = "Done!";
+
+      const testAsyncFn = (a, b, c) => {
+        expect(a).toBe(1);
+        expect(b).toBe(2);
+        expect(c).toBe(3);
+        return new Promise(resolve =>
+          setTimeout(() => {
+            resolve(returnedResults);
+          }, timeout)
+        );
+      };
+
+      vu.reportTx = ({ name, start, end, duration, error, data }) => {
+        expect(name).toBe(reportedName);
+        expect(duration).toBeGreaterThan(timeout);
+        expect(start).toBeLessThan(end);
+        expect(end).toBeLessThan(Date.now());
+        expect(error).toBe.false;
+        expect(data).toBe(returnedResults);
+        return data;
+      };
+
+      const res = await vu.txWrapper(reportedName, testAsyncFn, 1, 2, 3);
+      expect(res).toBe(returnedResults);
+    });
+  });
+
+  describe("reportTx", () => {
+    test("should not do anything if not ran as a child process", () => {
+      sinon.stub(process, "send");
+      vu.reportTx({ foo: "bar" });
+      expect(process.send.firstCall.args[0]).toEqual({
+        foo: "bar",
+        vu: 1337,
+        type: "TX"
+      });
+      process.send.restore();
     });
   });
 });
